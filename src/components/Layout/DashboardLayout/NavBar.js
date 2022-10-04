50ae1d5b4c2e0c4af43e3103ec8749c90b453898
Gabriel <marius_gabriel1998@yahoo.com>
2022-10-04 12:48:43 +0300

    misspelling correction

--- src/components/Layout/DashboardLayout/NavBar.js
+++ src/components/Layout/DashboardLayout/NavBar.js
@@ -27,7 +27,7 @@ const NavBar = (props) => {
     <>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Box>Gabriel Stoica - technical assesment</Box>
+          <Box>Gabriel Stoica - technical assessment</Box>
 
           <Flex alignItems={"center"}>
             <Stack direction={"row"} spacing={7}>
